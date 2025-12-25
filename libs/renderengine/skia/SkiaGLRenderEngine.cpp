59d8e6f91b227a10687bc114ead49d5b36ad69a4
greg <greg.jwaerhouse@gmail.com>
2025-12-25 13:13:33 -0500

    FIXUP: Use gl::GLExtensions instead of GLExtensions to fix the build.

--- libs/renderengine/skia/SkiaGLRenderEngine.cpp
+++ libs/renderengine/skia/SkiaGLRenderEngine.cpp
@@ -308,7 +308,7 @@ SkiaRenderEngine::Contexts SkiaGLRenderEngine::createContexts() {
 bool SkiaGLRenderEngine::supportsForwardPixelKill() const {
     // ARM gpu support this since 2013
     constexpr std::string kArm = "ARM";
-    return GLExtensions::getInstance().getVendor() == kArm;
+    return gl::GLExtensions::getInstance().getVendor() == kArm;
 }
 bool SkiaGLRenderEngine::supportsProtectedContentImpl() const {
     return mProtectedEGLContext != EGL_NO_CONTEXT;
